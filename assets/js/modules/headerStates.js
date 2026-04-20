d68cbcaf8f3ebed1baf88e5f435a403f0a88d5a7
projectnum17 <egor.chornyy@gmail.com>
2026-04-20 18:02:24 +0300

    layout search panel

--- assets/js/modules/headerStates.js
+++ assets/js/modules/headerStates.js
@@ -20,4 +20,50 @@ export const headerStates = () => {
 
     window.addEventListener('scroll', handleScroll);
     handleScroll();
+
+    const initSearchPanel = () => {
+        const trigger = document.querySelector('.js-search-trigger');
+        const searchPanel = document.querySelector('.js-search-panel');
+        if (!trigger || !searchPanel) return;
+
+        const searchField = searchPanel.querySelector('.js-search-field');
+        const closePanel = searchPanel.querySelector('.js-search-close');
+        const formSearch = searchPanel.querySelector('form');
+        if (!searchField || !closePanel || !formSearch) return;
+
+        trigger.addEventListener('click', (e) => {
+            e.stopPropagation();
+            document.body.classList.add('is-locked')
+            searchPanel.classList.add('is-show');
+            setTimeout(() => {
+                searchField.focus();
+            }, 100);
+        });
+
+        closePanel.addEventListener('click', (e) => {
+            e.stopPropagation();
+            searchPanel.classList.remove('is-show');
+            document.body.classList.remove('is-locked')
+
+            setTimeout(() => {
+                formSearch.reset();
+            }, 500);
+        });
+
+        const box = searchPanel.querySelector('.js-search-wrapper');
+        if (!box) return;
+
+        searchPanel.addEventListener('click', (e) => {
+            if (!box.contains(e.target)) {
+                searchPanel.classList.remove('is-show');
+                document.body.classList.remove('is-locked')
+
+                setTimeout(() => {
+                    formSearch.reset();
+                }, 500);
+            }
+        });
+    };
+
+    initSearchPanel();
 };
